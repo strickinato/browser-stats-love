919cda230fd80ab628d832957d2771a6471341d6
Toby Silverman <toby.silverman88@gmail.com>
2017-05-01 03:23:39 -0400

    emoji swag

--- source/javascripts/site.js
+++ source/javascripts/site.js
@@ -1,1 +1,36 @@
-// This is where it all goes :)
+var allEmojis;
+
+$(function(){
+
+  $('#heart-circle').click( function() {
+    var animationEnd = 'webkitAnimationEnd mozAnimationEnd MSAnimationEnd oanimationend animationend';
+    var randomNumber = Math.floor( Math.random() * allEmojis.length )
+
+    var emoji = allEmojis[randomNumber];
+
+    $("#heart").html(emoji);
+    $("#title-emoji").html(emoji);
+
+    $(this).addClass('animated bounceIn').one(animationEnd, function() {
+        $(this).removeClass('animated bounceIn');
+    });
+  });
+});
+
+allEmojis = [
+ '👹'
+,'🤷'
+,'😂'
+,'😍'
+,'🤔'
+,'😊'
+,'🔥'
+,'😘'
+,'🙄'
+, '💯'
+, '🖖'
+, '⚡️'
+, '🐐'
+, '💩'
+, '🙅‍♂️'
+];
