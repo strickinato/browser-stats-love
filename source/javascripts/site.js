2a78baf0e253bae5f691fbe13986e8add6560724
Ben <ben.a.pepin@gmail.com>
2017-05-24 18:00:03 -0400

    deleted fake buttons

--- source/javascripts/site.js
+++ source/javascripts/site.js
@@ -1,10 +1,21 @@
 var allEmojis;
 
 
+// Select toggler and put click handler
+$("#abutton").click(function() {
+
+  // In the handler, select the stuff you wanna flip
+  // And flip it
+  $("#browser-card").toggleClass("flipped")
+  $("#resolution-card").toggleClass("flippedplus")
+
+});
+
 
 
 $(function(){
-    $("#flip-button").click( function() {
+    $("#flip-tab-right").click( function() {
+     $("flip-tab").transition({ x: 200 })
     $("#browser-card").toggleClass("flipped")
     $("#resolution-card").toggleClass("flippedplus")
   });
